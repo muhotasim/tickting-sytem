0545645cbe296241dce05065f3b8257db3897197
Muhotasim Fuad <muhotasimF@gmail.com>
2024-04-06 12:51:18 +0600

    ticket controller added

--- backend/src/modules/ticketing-system/ticktet.module.ts
+++ backend/src/modules/ticketing-system/ticktet.module.ts
@@ -3,10 +3,11 @@ import { UserService } from "../common/services/user.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Ticket } from "src/models/ticket.model";
 import { CommonModule } from "../common/common.module";
+import { TicketController } from "./controller/ticket.controller";
 
 @Module({
     imports: [CommonModule,TypeOrmModule.forFeature([Ticket])],
-    controllers: [],
+    controllers: [TicketController],
     providers: [UserService]
 })
 export class TicketModule {
